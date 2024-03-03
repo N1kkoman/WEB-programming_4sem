39554ed774c801dafaf24d31b12c19cdc0a792dc
N1kkoman <assasinjok@gmail.com>
2024-03-03 20:46:03 +0200

    Add tasks 1-2 for laba2

--- 1/code/index.php
+++ 1/code/index.php
@@ -1,2 +1,33 @@
 <?php
-echo phpinfo();
+// 1
+/* Imagine a lot of code here */ 
+ $very_bad_unclear_name = "15 chicken wings";
+
+// Write your code here:
+
+$order =& $very_bad_unclear_name;
+$order .= " with ketchup";
+  
+ // Don't change the line below
+ echo "\nYour order is: $very_bad_unclear_name.";
+
+
+ // 2
+ $a = 1991;
+ echo $int_value;
+ 
+ echo "\n";
+ 
+ $float_value = 2.71;
+ echo $float_value;
+ 
+ echo "\n";
+ 
+ echo (20 - 8);
+
+ $last_month = 1187.23;
+ $this_month = 1089.98;
+ echo $last_month - $this_month;
+
+
+ 

6fcebc9cf422055b6af8044315e5f81ad6f26702
N1kkoman <assasinjok@gmail.com>
2024-03-08 13:01:16 +0200

    Minor changes to past tasks

--- 1/code/index.php
+++ 1/code/index.php
@@ -9,35 +9,34 @@ $order =& $very_bad_unclear_name;
 $order .= " with ketchup";
   
  // Don't change the line below
- echo "\nYour order is: $very_bad_unclear_name.";
+ echo "Your order is: $very_bad_unclear_name.";
 
 
+echo "<br>".'----------------'."<br>";
  // 2
  $a = 1991;
- echo $int_value;
- 
- echo "\n";
- 
+ echo $a. "<br>";
+
  $float_value = 2.71;
- echo $float_value;
- 
- echo "\n";
- 
- echo (20 - 8);
+ echo $float_value. "<br>";
+
+ echo (20 - 8). "<br>";
 
  $last_month = 1187.23;
  $this_month = 1089.98;
- echo $last_month - $this_month;
+ echo $last_month - $this_month. "<br>";
 
 
+echo "<br>".'----------------'."<br>";
  // 11
 $num_languages = 4;
 $months = 11;
 $days = $months * 16;
 $days_per_language = $days / $num_languages;
-echo $days_per_language . "\n";
+echo $days_per_language . "<br>";
 
 
+echo "<br>".'----------------'."<br>";
 // 12
-echo 8 ** 2 . "\n";
+echo 8 ** 2 . "<br>";
 
